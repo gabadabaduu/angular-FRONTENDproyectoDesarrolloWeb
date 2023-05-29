a24c2ae3aaf6a3c00ed88bc7b080c25b6758a052
gabriela luigi <luigigabriela68@gmail.com>
2023-05-28 20:11:14 -0500

    pequeños cambios

--- src/app/Pages/Usuario/usuario-forms/usuario-forms.component.ts
+++ src/app/Pages/Usuario/usuario-forms/usuario-forms.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { UserControllerService } from 'src/app/Services/usuario/user-controller.service';
 import { Usuario } from 'src/app/Modelo/usuario';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-usuario-forms',
@@ -8,22 +10,28 @@ import { Usuario } from 'src/app/Modelo/usuario';
   styleUrls: ['./usuario-forms.component.css']
 })
 export class UsuarioFormsComponent {
+  showErrorMessage: boolean = false;
+  errorMessage: string = '';
 
-constructor(private UserController : UserControllerService){}
+constructor(private UserController : UserControllerService, private router: Router, private http: HttpClient){}
 
   model = new Usuario(18, 'Usuario', 'Contraseña');
   
   submitted = false;
   
   onSubmit() {
-    this.UserController.login(this.model.name, this.model.password)
+    this.http.post(this.model.name, this.model.password)
       .subscribe(
         response => {
-          // Manejar la respuesta del backend en caso de éxito
+          console.log(response);
+        this.router.navigate(['/Canchas']);
           
         },
         error => {
           // Manejar el error en caso de fallo
+          console.error(error);
+          this.showErrorMessage = true;
+          this.errorMessage = 'Error al crear la cancha. Por favor, inténtelo nuevamente.';
         }
       );
   }
@@ -34,12 +42,7 @@ constructor(private UserController : UserControllerService){}
   this.model = new Usuario(42,'Usuario','Contraseña');
   }
   
-  VUsuario(): Usuario {
-  const myUsuario =  new Usuario(42, 'Maria',
-                     '12345678');
-  console.log('Bienvenido ' + myUsuario.name); // "My hero is called SkyDog"
-  return myUsuario;
-  }
+ 
   
   
 }
